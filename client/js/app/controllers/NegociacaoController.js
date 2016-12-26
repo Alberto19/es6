9bfa45c24fcce73a4b501e3550372008a4765faa
Junior <jose.farias@itst.com.br>
2016-12-26 14:23:38 -0200

    Criando uma lista de negociacoes com programacao defensiva

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,19 +6,31 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event) {
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegocicao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
+        // console.log(DateHelper.dataParaTexto(negociacao.data));
+
+    }
+
+    _criaNegocicao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
+            this._inputValor.value);
+    }
 
-        console.log(DateHelper.dataParaTexto(negociacao.data));
-
+    _limpaFormulario() {
+        this._inputData.value = "";
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,21 +1,22 @@
 class DateHelper {
 
-    constructor(){
+    constructor() {
         throw new Error('DateHelper nao pode ser instanciada');
     }
 
     static dataParaTexto(data) {
-         return data.getDate() 
-        + '/' + (data.getMonth() +1)
-        + '/' + data.getFullYear();
-        }
+        return `${data.getDate()}/${data.getMonth() +1}/${data.getFullYear()}`;
+    }
 
     static textoParaData(texto) {
-        return new Date(...texto
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-    }
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
+            return new Date(...texto
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+            );
+        }
         // let data = new Date(this._inputData.value.split('-'));
         // let data = new Date(this._inputData.replace(/-/g, ','));
         // let data = new Date(...
