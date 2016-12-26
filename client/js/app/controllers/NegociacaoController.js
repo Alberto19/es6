ac9565f24f52778920ffa284722660a183aab62e
Junior <jose.farias@itst.com.br>
2016-12-26 15:32:49 -0200

    Criando a View
    Utilizando funcoes: Map, Join, Reduce 
    Um pouco de IIFE, Rederizando a view Pelo TemplateString, tranformando html em elemento do Dom

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,6 +7,9 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
 
     }
 
@@ -14,8 +17,9 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegocicao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
-        console.log(this._listaNegociacoes.negociacoes);
+        // console.log(this._listaNegociacoes.negociacoes);
         // console.log(DateHelper.dataParaTexto(negociacao.data));
 
     }
