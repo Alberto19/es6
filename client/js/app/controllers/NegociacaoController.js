139012805a3cc07a9e6b7027ef9d1041ab288e54
Junior <jose.farias@itst.com.br>
2016-12-27 11:09:07 -0200

    Api reflect   e Arrow Function Lexico
    reflect.aply parra o this correto para a funcao chamada

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,10 +6,11 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model =>
+            this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._negociacoesView.update(this._listaNegociacoes);   
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
@@ -20,7 +21,6 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegocicao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = "Negociação adicionada com sucesso";
         this._mensagemView.update(this._mensagem);
@@ -29,6 +29,13 @@ class NegociacaoController {
         // console.log(this._listaNegociacoes.negociacoes);
         // console.log(DateHelper.dataParaTexto(negociacao.data));
 
+    }
+
+    apaga(){
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto ='Negociações apagadas com sucesso!';
+        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegocicao() {
